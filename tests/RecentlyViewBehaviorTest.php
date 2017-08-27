e7a53cf07437814c9b2308fc9c50debcfa2dafc5
zacksleo <zacksleo@gmail.com>
2017-08-27 23:12:59 +0800

    tests

--- tests/RecentlyViewBehaviorTest.php
+++ tests/RecentlyViewBehaviorTest.php
@@ -10,12 +10,14 @@ class RecentlyViewBehaviorTest extends TestCase
     public function testBehavior()
     {
         \Yii::$app->getSession()->removeAll();
+        $_SESSION = [];
         $model = Item::findOne(1);
         $behavior = new RecentlyViewedBehavior();
         $behavior->limit = 5;
         $behavior->setRecentlyViewed(get_class($model), $model->id);
         $models = $behavior->getRecentlyViewed(get_class($model));
         $this->assertInstanceOf('tests\data\models\Item', $models[0]);
+        var_dump($models);
         $this->assertEquals(1, count($models));
     }
 }
